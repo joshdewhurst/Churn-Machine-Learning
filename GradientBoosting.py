9a814c50b97521e35c060b3f95e03bb44c2b323f
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 11:10:11 -0700

    Create GradientBoosting.py

---
+++ GradientBoosting.py
@@ -0,0 +1,27 @@
+# Import required libraries
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+# Load the data
+data = pd.read_csv('customer_data.csv')
+
+# Select the features and target variable
+X = data[['usage_frequency', 'account_status', 'other_behavioral_data']]
+y = data['churn']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit the gradient boosting model
+gb = GradientBoostingClassifier(n_estimators=100, max_depth=5, random_state=42)
+gb.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = gb.predict(X_test)
+
+# Evaluate the model's performance
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
