1e3960fa3c19a437cf339c2fc6846411d71aa810
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 11:18:27 -0700

    Update NuetralNetwork.py

--- NuetralNetwork.py
+++ NuetralNetwork.py
@@ -1,9 +1,8 @@
 # Import required libraries
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.neural_network import MLPClassifier
 
 # Load the data
 data = np.loadtxt("customer_data.csv", delimiter=",")
@@ -16,21 +15,16 @@ y = data[:,3]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define the neural network architecture
-model = Sequential()
-model.add(Dense(8, input_dim=3, activation='relu'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-# Compile the model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model = MLPClassifier(hidden_layer_sizes=(8, 4), activation='relu', solver='adam', random_state=42)
 
 # Fit the model to the training data
-model.fit(X_train, y_train, epochs=50, batch_size=32)
+model.fit(X_train, y_train)
 
 # Make predictions on the test set
-y_pred = model.predict_classes(X_test)
+y_pred = model.predict(X_test)
 
 # Evaluate the model's performance
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
+
