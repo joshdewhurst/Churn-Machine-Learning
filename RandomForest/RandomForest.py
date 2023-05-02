2e1c2f55ff5ed8e017bb4bd7baa3fa89f6eb4cd0
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 16:10:24 -0700

    Update RandomForest.py

--- RandomForest/RandomForest.py
+++ RandomForest/RandomForest.py
@@ -27,3 +27,7 @@ y_pred = rf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
