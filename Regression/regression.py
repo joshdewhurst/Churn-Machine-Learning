1bebf83d99a0817189adafabe42dfae9265fd367
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 15:21:39 -0700

    Update regression.py

--- Regression/regression.py
+++ Regression/regression.py
@@ -1,37 +1,36 @@
+# Import required libraries
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score
+
 
 # Load the data
 data = pd.read_csv('customer_data.csv')
 
-# Map the 'Yes' and 'No' values to 1 and 0 for the 'status' column
 data['status'] = data['status'].map({'Yes': 1, 'No': 0})
+
 
 # Select the features and target variable
 X = data[['usage_frequency', 'status', 'other_behavioral_data']]
+y = data['churn']
 
-# Fit the logistic regression model on the entire dataset
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit the logistic regression model
 lr = LogisticRegression()
-lr.fit(X, data['churn'])
+lr.fit(X_train, y_train)
 
-# Make predictions on the dataset
-y_pred = lr.predict(X)
-
-# Add the predicted churn values to the DataFrame as a new column
-data['churn_prediction'] = y_pred
-
-# Write the updated DataFrame to a new CSV file
-data.to_csv('customer_data_predicted.csv', index=False)
+# Make predictions on the test set
+y_pred = lr.predict(X_test)
 
 # Evaluate the model's performance
 accuracy = lr.score(X_test, y_test)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 
-# Print the predicted values
-print(y_pred)
-
 print("Accuracy:", accuracy)
 print("Precision:", precision)
 print("Recall:", recall)
+
