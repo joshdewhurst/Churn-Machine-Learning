2073be7c0bc3980a002e55ceb0a21be8051f18bf
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 11:06:35 -0700

    Create RandomForest.py

---
+++ RandomForest.py
@@ -0,0 +1,27 @@
+# Import required libraries
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+# Load the data
+data = pd.read_csv('customer_data.csv')
+
+# Select the features and target variable
+X = data[['usage_frequency', 'account_status', 'other_behavioral_data']]
+y = data['churn']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Fit the random forest model
+rf = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+rf.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = rf.predict(X_test)
+
+# Evaluate the model's performance
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
