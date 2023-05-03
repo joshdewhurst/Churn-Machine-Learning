8c395629758851a126da01d5782d8ed44fcdc195
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 18:06:04 -0700

    Update DecisionTree.py

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -28,3 +28,7 @@ y_pred = dt.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Precision:" precision)
+print("Recal:" recall)
