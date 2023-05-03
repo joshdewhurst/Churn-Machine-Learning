885cd07494100cc173ffd47441c96156ca9228ed
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-05-02 17:02:32 -0700

    Update NuetralNetwork.py

--- NuetralNetwork/NuetralNetwork.py
+++ NuetralNetwork/NuetralNetwork.py
@@ -1,15 +1,18 @@
-# Import required libraries
+import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.neural_network import MLPClassifier
 
-# Load the data, skip the header row
-data = np.loadtxt("customer_data.csv", delimiter=",", skiprows=1)
+# Load the data using pandas
+df = pd.read_csv("customer_data.csv")
 
-# Select the features and target variable
-X = data[:, 0:3]
-y = data[:, 3]
+# Encode the categorical variable using one-hot encoding
+df = pd.get_dummies(df, columns=['status'])
+
+# Split the data into features and target variable
+X = df.drop('churn', axis=1).values
+y = df['churn'].values
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -30,4 +33,4 @@ recall = recall_score(y_test, y_pred)
 
 print("Accuracy:", accuracy)
 print("Precision:", precision)
-print("Recall:", accuracy)
+print("Recall:", recall)
